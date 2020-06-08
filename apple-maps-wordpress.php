0aa334c8079122146e77418eaa6f9883413d9080
Jeffrey Paul <jeffpaul@hotmail.com>
2020-06-07 21:27:09 -0500

    Update apple-maps-wordpress.php

--- apple-maps-wordpress.php
+++ apple-maps-wordpress.php
@@ -4,12 +4,12 @@
  * Plugin URI:        https://github.com/10up/apple-maps-wordpress
  * Description:       
  * Version:           0.1.0
- * Requires at least: 
- * Requires PHP:      
+ * Requires at least: 5.2
+ * Requires PHP:      5.6
  * Author:            10up
  * Author URI:        https://10up.com
- * License:           
- * License URI:       
+ * License:           GPLv2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       apple-maps-wordpress
  * Domain Path:       /languages
  *
