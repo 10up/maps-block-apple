3211d4da63ed4380ace113e3e5cca6afe5651b27
Ryan Welcher <ryan.welcher@10up.com>
2020-06-23 15:05:35 -0400

    Remove unused import.

--- src/inspector-settings.js
+++ src/inspector-settings.js
@@ -2,7 +2,6 @@ import { InspectorControls } from '@wordpress/block-editor';
 import { PanelBody } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
-import AuthenticationSettings from './Settings/AuthenticationSettings';
 import DisplaySettings from './Settings/DisplaySettings';
 import LocationSettings from './Settings/LocationSettings';
 
