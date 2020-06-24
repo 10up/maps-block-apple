2c3335a4f4ceacc1099e80468083498aa9e1086c
Helen Hou-Sandi <helen.y.hou@gmail.com>
2020-06-24 11:46:21 -0600

    Block activation on < WP 5.2
    This also removes the lang dir header, unnecessary with lang packs

--- maps-block-apple.php
+++ maps-block-apple.php
@@ -11,7 +11,6 @@
  * License:           GPLv2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       maps-block-apple
- * Domain Path:       /languages
  *
  * @package           tenup\Maps_Block_Apple
  */
@@ -25,7 +24,21 @@ define( 'MAPS_BLOCK_APPLE_PATH', dirname( __FILE__ ) . '/' );
 define( 'MAPS_BLOCK_APPLE_INC', MAPS_BLOCK_APPLE_PATH . 'includes/' );
 define( 'MAPS_BLOCK_APPLE_BASENAME', plugin_basename( __FILE__ ) );
 
-add_action( 'init', __NAMESPACE__ . '\add_options' );
+/**
+ * Require PHP version 5.6 - throw an error if the plugin is activated on an older version.
+ */
+register_activation_hook(
+	__FILE__,
+	function() {
+		if ( ! version_compare( $GLOBALS['wp_version'], '5.2', '>=' ) ) {
+			wp_die(
+				esc_html__( 'Block for Apple Maps requires WordPress version 5.2 or greater.', 'maps-block-apple' ),
+				esc_html__( 'Error Activating', 'maps-block-apple' )
+			);
+		}
+	}
+);
+
 /**
  * Add options
  */
@@ -34,6 +47,7 @@ function add_options() {
 	add_option( 'maps_block_apple_key_id' );
 	add_option( 'maps_block_apple_private_key' );
 }
+add_action( 'init', __NAMESPACE__ . '\add_options' );
 
 require_once MAPS_BLOCK_APPLE_INC . 'block_assets.php';
 require_once MAPS_BLOCK_APPLE_INC . 'rest_routes.php';
