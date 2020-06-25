2ef553d1283e7d8258fdfcc61662b0ddbff17add
Helen Hou-Sandi <helen.y.hou@gmail.com>
2020-06-25 14:45:07 -0600

    Fix lint error

--- src/components/LocationInfo.js
+++ src/components/LocationInfo.js
@@ -3,9 +3,10 @@ import { __ } from '@wordpress/i18n';
 export default function LocationInfo( { latitude, longitude } ) {
 	return (
 		<>
-			<p>{ __( 'Coordinates:', 'maps-block-apple' ) } { `${ latitude.toFixed(
-				4
-			) }, ${ longitude.toFixed( 4 ) }` }</p>
+			<p>
+				{ __( 'Coordinates: ', 'maps-block-apple' ) }
+				{ `${ latitude.toFixed( 4 ) }, ${ longitude.toFixed( 4 ) }` }
+			</p>
 		</>
 	);
 }
