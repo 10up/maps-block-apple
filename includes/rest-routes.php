fcd206020222c10bc0f13d1f2d36a76a879e48cd
Fabian Kägy <fabian.kaegy@10up.com>
2021-10-11 15:30:04 +0200

    add inline documentation

--- includes/rest-routes.php
+++ includes/rest-routes.php
@@ -89,6 +89,9 @@ function get_jwt() {
 		'origin' => get_fqdn_from_url( get_site_url() ),
 	];
 
+	// exlude the origin restriction from the JWT for local environemts
+	// this is to allow tools like wp-env or browsersync to work since the url
+	// is not the same as the site url.
 	if ( wp_get_environment_type() === 'local' ) {
 		unset( $body['origin'] );
 	}
