5f38f1feb739cc41edbe564fab77c19f564cd337
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-05-12 15:32:17 +0300

    Command to save settings

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -23,3 +23,30 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add(
+	"mapsBlockSaveSettings",
+	(
+		key = Cypress.env("MAPS_PRIVATE_KEY"),
+		key_id = Cypress.env("MAPS_KEY_ID"),
+		team_id = Cypress.env("MAPS_TEAM_ID")
+	) => {
+		cy.visit(`/wp-admin/options-general.php?page=block-for-apple-maps`);
+		cy.get("#token-gen-authkey").click().clear();
+		if (key) {
+			cy.get("#token-gen-authkey").type(key);
+		}
+
+		cy.get("#token-gen-kid").click().clear();
+		if (key_id) {
+			cy.get("#token-gen-kid").click().clear().type(key_id);
+		}
+
+		cy.get("#token-gen-iss").click().clear();
+		if (team_id) {
+			cy.get("#token-gen-iss").click().clear().type(team_id);
+		}
+
+		cy.get("#submit").click();
+	}
+);
